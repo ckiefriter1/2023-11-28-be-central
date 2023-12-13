4bf26e8f93d4f7bc94b47992506087f445ce61e0
Chuck Kiefriter <ckiefriter@gmail.com>
2023-12-12 20:45:42 -0500

    first commit

---
+++ src/week1/HelloWorld.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Single line comment
+		
+		/*
+		 * multi-line
+		 * comment
+		 */
+
+		System.out.println("Hello World");
+	}
+
+}
